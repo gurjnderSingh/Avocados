bcae6476dd0540a6cfc97bd1b2bdcc5e69a5980d
gurjnderSingh <gurjindersingh663@gmail.com>
2023-02-12 10:08:45 +0530

    Initial project created

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Gurjinder Singh on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
